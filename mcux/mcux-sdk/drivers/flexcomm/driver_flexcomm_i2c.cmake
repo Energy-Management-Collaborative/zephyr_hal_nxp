f47ca0b790ed7309c62c8c4685f79a4b5d4cb5fa
Mitchel Haan <mitchel@verypossible.com>
2022-11-09 15:22:18 -0500

    Enable Flexcomm I2C timeout/recovery

--- mcux/mcux-sdk/drivers/flexcomm/driver_flexcomm_i2c.cmake
+++ mcux/mcux-sdk/drivers/flexcomm/driver_flexcomm_i2c.cmake
@@ -6,6 +6,12 @@ target_sources(${MCUX_SDK_PROJECT_NAME} PRIVATE
     ${CMAKE_CURRENT_LIST_DIR}/fsl_i2c.c
 )
 
+# Enable I2C timeout recovery and limit status polling
+target_compile_definitions(${MCUX_SDK_PROJECT_NAME} PRIVATE
+    FSL_FEATURE_I2C_TIMEOUT_RECOVERY=y
+    I2C_RETRY_TIMES=3
+)
+
 target_include_directories(${MCUX_SDK_PROJECT_NAME} PUBLIC
     ${CMAKE_CURRENT_LIST_DIR}/.
 )
